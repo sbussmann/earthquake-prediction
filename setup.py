5d7b978fd8a4e25e07e6729ebb3abf508b1eb72d
Shane Bussmann <sbussmann@gmail.com>
2019-03-22 22:05:43 -0400

    Initial commit with cookie cutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict when earthquakes will occur.',
+    author='Shane Bussmann',
+    license='MIT',
+)
